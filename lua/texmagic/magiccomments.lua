3c82af2ee0b11a99b0fd47976046396ea5439f19
Jake Vincent <jakewvincent@gmail.com>
2021-06-11 15:25:35 -0700

    make default value for tex_program pdflatex

--- lua/texmagic/magiccomments.lua
+++ lua/texmagic/magiccomments.lua
@@ -1,12 +1,10 @@
 local M = {}
 
-local magic = {}                                                -- table for storing magic comments
-local tex_program                                               -- TeX program variable
-
 M.findMagicComments = function()
-    local cur_file = vim.api.nvim_buf_get_name(0)               -- get name of current file
-    if string.find(cur_file, '%.tex') ~= nil                    -- if current file has tex extension, ...
-    then
+    if vim.bo.filetype == "tex" then                            -- if current file has tex extension, ...
+        local cur_file = vim.api.nvim_buf_get_name(0)           -- get name of current file
+        local magic = {}                                        -- table for storing magic comments
+        local tex_program = "pdflatex"
         local tex_file = io.open(cur_file, "r")                 -- lua open tex file in read mode
         local line = tex_file:read()                            -- grab first line
         local i = 1                                             -- initialize counter variable
@@ -16,16 +14,16 @@ M.findMagicComments = function()
             line = tex_file:read()                              -- get the next line
         end
         tex_file:close()                                        -- close the file
---        print(table.concat(magic))                              -- [for testing]
-    end
-    -- set some variables based on the magic comments
-    for i = 1, #magic, 1 do
-        if string.find(magic[i], "program") ~= nil then
-            local start = string.find(magic[i], "=") + 1
-            tex_program = string.lower(string.gsub(string.sub(magic[i], start), "%s+", ""))
+        for i = 1, #magic, 1 do
+            if string.find(magic[i], "program") ~= nil then
+                local start = string.find(magic[i], "=") + 1
+                local tex_program = string.lower(string.gsub(string.sub(magic[i], start), "%s+", ""))
+            end
+            return{tex_program = tex_program}
         end
+    else
+        return{tex_program = "pdflatex"}
     end
-    return{tex_program = tex_program}
 end
 
 M.texProgram = function()
