ed2da894a61c1e9410c218a51e36cb57a56654dd
Jake Vincent <jakewvincent@gmail.com>
2021-06-11 15:32:00 -0700

    better comments

--- lua/texmagic/magiccomments.lua
+++ lua/texmagic/magiccomments.lua
@@ -8,25 +8,31 @@ M.findMagicComments = function()
         local tex_file = io.open(cur_file, "r")                 -- lua.open tex file in read mode
         local line = tex_file:read()                            -- grab first line
         while string.find(line, '%%%s*!%s*TEX') ~= nil do       -- as long as line has a magic tex comment, ...
-            table.insert(magic, line)                           -- add it to table
+            table.insert(magic, line)                           -- add it to magic table
             line = tex_file:read()                              -- get the next line
         end
         tex_file:close()                                        -- close the file
         for i = 1, #magic, 1 do
-            if string.find(magic[i], "program") ~= nil then
-                local start = string.find(magic[i], "=") + 1
-                local tex_program = string.lower(string.gsub(string.sub(magic[i], start), "%s+", ""))
+            if string.find(magic[i], "program") ~= nil then     -- if a cell in the table contains the word "program", ...
+                local start = string.find(magic[i], "=") + 1    -- get the index of the = sign
+                local tex_program = string.lower(               -- get the stuff after =, converting it to lowercase, ...
+                    string.gsub(
+                        string.sub(
+                            magic[i], start
+                        ), "%s+", ""                            -- and stripping whitespace
+                    )
+                )
             end
-            return{tex_program = tex_program}
+            return{tex_program = tex_program}                   -- return a table
         end
     else
-        return{tex_program = "pdflatex"}
+        return{tex_program = "pdflatex"}                        -- otherwise, use return the same table but w/ the default value
     end
 end
 
 M.texProgram = function()
     --print(M.findMagicComments()["tex_program"])               -- test
-    return(M.findMagicComments()["tex_program"])
+    return(M.findMagicComments()["tex_program"])                -- return the tex program
 end
 
 return M
